36a95939764872eb2bb1dab79043ae52644b6682
sumit <itssumit258@gmail.com>
2023-10-26 22:04:04 +0530

    feat. trasnparent bottom menu and card hover effect

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 const side_bar = document.getElementById("side_bar");
 const menu_icon = document.getElementById("menu_icon");
 const sidebar_close_button = document.getElementById("sidebar_close_button");
-
+let bottom_menu_items = document.getElementById("bottom_menu_items");
 // toggle mobile menu
 menu_icon.addEventListener("click", () => {
   side_bar.style.display = "flex";
@@ -9,4 +9,17 @@ menu_icon.addEventListener("click", () => {
 
 sidebar_close_button.addEventListener("click", () => {
   side_bar.style.display = "none";
+});
+
+bottom_menu_items = Array.from(bottom_menu_items.children);
+bottom_menu_items.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    item.firstElementChild.style.width = "55px";
+    item.firstElementChild.style.height = "55px";
+  });
+
+  item.addEventListener("mouseleave", () => {
+    item.firstElementChild.style.width = "30px";
+    item.firstElementChild.style.height = "30px";
+  });
 });
